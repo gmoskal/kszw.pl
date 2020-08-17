fba2343083ea46dbae6a4ee5b9d577e7c62095fa
gmoskal <g.moskal@gmail.com>
2020-08-18 01:37:36 +0200

    safari hex color

--- utils/colors.ts
+++ utils/colors.ts
@@ -5,17 +5,17 @@ export type ColorParam = 0 | 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100
 export type ColorFn<T> = (v: keyof T, p: ColorParam) => string
 
 export const init = <T>() => {
-    const toCSSRgb = (cs: [number, number, number]) => `rgb(${cs.map(c => Math.round(c * 255)).join(", ")})`
+    const rgbToHex = ([r, g, b]: [number, number, number]) =>
+        "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
 
     return {
         withLightness: (t: T): ColorFn<T> => (v, l) => {
             const [h, s] = hexToHpluv(`${t[v]}`)
-            return toCSSRgb(hsluvToRgb([h, s, l]))
+            return rgbToHex(hsluvToRgb([h, s, l]))
         },
         withSaturation: (t: T): ColorFn<T> => (v, s) => {
             const [h, _, l] = hexToHpluv(`${t[v]}`)
-            return toCSSRgb(hsluvToRgb([h, s, l]))
-        },
-        toCSSRgb
+            return rgbToHex(hsluvToRgb([h, s, l]))
+        }
     }
 }
